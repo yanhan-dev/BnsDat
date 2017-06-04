59231b892f9949011b07a6e92fe16e7bc15de1ee
烟寒 <luanfengjie@hotmail.com>
2017-06-04 16:09:26 +0800

    1.0
    BnsDat CSharp

---
+++ BnsDat/BnsDat/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ProgressBarSolution;
+
+namespace BnsDat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //无参数调用本程序
+            if (args.Length != 2)
+            {
+                Usage();
+                Console.ReadKey();
+                return;
+            }
+
+            BNSDat bnsdat = new BNSDat();
+
+            if (!File.Exists("Ionic.Zlib.dll"))
+            {
+                File.WriteAllBytes(Directory.GetCurrentDirectory()+ "\\Ionic.Zlib.dll", Resource_lib.Ionic_Zlib);
+            }
+
+            if (args[0] == "-e")
+            {
+                Console.Write("正在解包: ");
+                ProgressBar progressBar = new ProgressBar(Console.CursorLeft, Console.CursorTop, 50, ProgressBarType.Multicolor);
+                bnsdat.Extract(args[1], args[1].Contains("64"), progressBar.Dispaly);
+                Console.WriteLine("  解包完成");
+            }
+            else if (args[0] == "-c")
+            {
+                Console.Write("正在打包: ");
+                ProgressBar progressBar = new ProgressBar(Console.CursorLeft, Console.CursorTop, 50, ProgressBarType.Multicolor);
+                bnsdat.Compress(args[1], args[1].Contains("64"), progressBar.Dispaly);
+                Console.WriteLine("  打包完成");
+            }
+            else
+            {
+                Console.WriteLine("命令无效");
+                Usage();
+                return;
+            }
+            
+            
+        }
+        public static void Usage()
+        {
+            Console.WriteLine("\n调用方法: bnsdat.exe [命令] [文件(夹)]\n");
+            Console.WriteLine("命令：\n");
+            Console.WriteLine("  -e 文件 \t解包 xml.dat/config.dat/xml64.dat/config64.dat 到相应文件夹，例如 xml.dat.files \n");
+            Console.WriteLine("  -c 文件夹 \t打包 xml.dat.files/config.dat.files/xml64.dat.files/config64.dat.files 为相应文件，例如 xml.dat \n");
+            Console.WriteLine("\nBy 烟寒: https://github.com/laoluan/bnsdat \n");
+        }
+    }
+
+    
+
+}
---
+++ BnsDat/BnsDat/ProgressBar.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace ProgressBarSolution
+{
+    /// <summary>
+    /// 进度条类型
+    /// </summary>
+    public enum ProgressBarType
+    {
+        /// <summary>
+        /// 字符
+        /// </summary>
+        Character,
+        /// <summary>
+        /// 彩色
+        /// </summary>
+        Multicolor
+    }
+
+    public class ProgressBar
+    {
+
+        /// <summary>
+        /// 光标的列位置。将从 0 开始从左到右对列进行编号。
+        /// </summary>
+        public int Left { get; set; }
+        /// <summary>
+        /// 光标的行位置。从上到下，从 0 开始为行编号。
+        /// </summary>
+        public int Top { get; set; }
+
+        /// <summary>
+        /// 进度条宽度。
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// 进度条当前值。
+        /// </summary>
+        public int Value { get; set; }
+        /// <summary>
+        /// 进度条类型
+        /// </summary>
+        public ProgressBarType ProgressBarType { get; set; }
+
+
+        private ConsoleColor colorBack;
+        private ConsoleColor colorFore;
+
+
+        public ProgressBar() : this(Console.CursorLeft, Console.CursorTop)
+        {
+
+        }
+
+        public ProgressBar(int left, int top, int width = 50, ProgressBarType progressBarType = ProgressBarType.Multicolor)
+        {
+            this.Left = left;
+            this.Top = top;
+            this.Width = width;
+            this.ProgressBarType = progressBarType;
+
+            // 清空显示区域；
+            Console.SetCursorPosition(Left, Top);
+            for (int i = left; ++i < Console.WindowWidth;) { Console.Write(" "); }
+
+            if (this.ProgressBarType == ProgressBarType.Multicolor)
+            {
+                // 绘制进度条背景； 
+                colorBack = Console.BackgroundColor;
+                Console.SetCursorPosition(Left, Top);
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
+                for (int i = 0; ++i <= width;) { Console.Write(" "); }
+                Console.BackgroundColor = colorBack;
+            }
+            else
+            {
+                // 绘制进度条背景；    
+                Console.SetCursorPosition(left, top);
+                Console.Write("[");
+                Console.SetCursorPosition(left + width - 1, top);
+                Console.Write("]");
+            }
+        }
+
+        public int Dispaly(int value)
+        {
+            return Dispaly(value, null);
+        }
+
+        public int Dispaly(int value, string msg)
+        {
+            if (this.Value != value)
+            {
+                this.Value = value;
+
+                if (this.ProgressBarType == ProgressBarType.Multicolor)
+                {
+                    // 保存背景色与前景色；
+                    colorBack = Console.BackgroundColor;
+                    colorFore = Console.ForegroundColor;
+                    // 绘制进度条进度                
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.SetCursorPosition(this.Left, this.Top);
+                    Console.Write(new string(' ', (int)Math.Round(this.Value / (100.0 / this.Width))));
+                    Console.BackgroundColor = colorBack;
+
+                    // 更新进度百分比,原理同上.                 
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.SetCursorPosition(this.Left + this.Width + 1, this.Top);
+                    if (string.IsNullOrWhiteSpace(msg)) { Console.Write("{0}%", this.Value); } else { Console.Write(msg); }
+                    Console.ForegroundColor = colorFore;
+                }
+                else
+                {
+                    // 绘制进度条进度      
+                    Console.SetCursorPosition(this.Left + 1, this.Top);
+                    Console.Write(new string('*', (int)Math.Round(this.Value / (100.0 / (this.Width - 2)))));
+                    // 显示百分比   
+                    Console.SetCursorPosition(this.Left + this.Width + 1, this.Top);
+                    if (string.IsNullOrWhiteSpace(msg)) { Console.Write("{0}%", this.Value); } else { Console.Write(msg); }
+                }
+            }
+            return value;
+        }
+    }
+}
